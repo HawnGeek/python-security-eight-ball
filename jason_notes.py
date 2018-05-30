e2f2f428faf029914396dadc3043ff3f2dc04111
eric <user@github.com>
2018-05-29 18:59:12 -1000

    class notes

--- jason_notes.py
+++ jason_notes.py
@@ -1,12 +1,13 @@
 import socket
 import _thread #import all functions from the thread library by their own name
+
 host = '127.0.0.1'
 port = 4444
 
 def clientthread(conn):
     #Send a message back to the user that connected over this socket connection
     conn.send(b'You have summoned the Security Eight Ball, what is your question?\n')
-   
+
     while True:
         #Receive new messages from the client
         data = conn.recv(1024)
@@ -24,15 +25,15 @@ def create_server():
     # listen and accept a max of 5 connections
     s.listen(5)
 
-
+    print('look at me, i go')
     while True:
-    # Wait for someone to try to connect and accept the connection
-    c, addr = s.accept()
-    print('Got connection from', addr)
-    # send a response
-    _thread.start_new_thread(clientthread, (c, ))
+        # Wait for someone to try to connect and accept the connection
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        # send a response
+        _thread.start_new_thread(clientthread, (c, ))
     # close the connection
     c.close()
 
 if __name__ == '__main__':
-  create_server()
+    create_server()
